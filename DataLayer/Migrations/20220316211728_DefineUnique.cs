8667ab27d87f039c93657d09e5bbcbb5b2516485
ORBIS ALONZO GUTIERREZ <orbis_ag@hotmail.com>
2022-03-16 17:44:28 -0400

    fix: Unique colunms in the db - New migration was generated - The EFConfiguration was updated for support a unique - State and Country controllers are updated

---
+++ DataLayer/Migrations/20220316211728_DefineUnique.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataLayer.Migrations
+{
+    public partial class DefineUnique : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_States_Code",
+                table: "States",
+                column: "Code",
+                unique: true,
+                filter: "IsDeleted = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_States_Name",
+                table: "States",
+                column: "Name",
+                unique: true,
+                filter: "IsDeleted = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Alpha2Code",
+                table: "Countries",
+                column: "Alpha2Code",
+                unique: true,
+                filter: "IsDeleted = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Alpha3Code",
+                table: "Countries",
+                column: "Alpha3Code",
+                unique: true,
+                filter: "IsDeleted = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_Name",
+                table: "Countries",
+                column: "Name",
+                unique: true,
+                filter: "IsDeleted = 0");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Countries_NumberCode",
+                table: "Countries",
+                column: "NumberCode",
+                unique: true,
+                filter: "IsDeleted = 0");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_States_Code",
+                table: "States");
+
+            migrationBuilder.DropIndex(
+                name: "IX_States_Name",
+                table: "States");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_Alpha2Code",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_Alpha3Code",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_Name",
+                table: "Countries");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Countries_NumberCode",
+                table: "Countries");
+        }
+    }
+}
--- Tsc.Api/Controllers/Countries/CountriesController.cs
+++ Tsc.Api/Controllers/Countries/CountriesController.cs
@@ -4,9 +4,23 @@
     [ApiController]
     public class CountriesController : CoreController<ICountryService, CountryMapper, CountryInputMapper, CountryEditMapper>
     {
+        private readonly ICountryService _countryService;
         public CountriesController(ICountryService service) : base(service)
         {
+            _countryService = service;
+        }
 
+        public override async Task<IActionResult> Create(CountryInputMapper inputModel, CancellationToken cancellationToken = default)
+        {
+            var existOne = await _countryService.Exist(x => x.Name == inputModel.Name 
+            || x.Alpha3Code == inputModel.Alpha3Code
+            || x.Alpha2Code == inputModel.Alpha2Code
+            || x.NumberCode == inputModel.NumberCode,cancellationToken);
+
+            if (existOne)
+                return BadRequest("The entity exist, all property are unique");
+
+            return await base.Create(inputModel, cancellationToken);
         }
     }
 }
